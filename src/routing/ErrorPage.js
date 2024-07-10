c815d1df7904f4443fc04136817c842adbad8083
ndjerrou <n.djerrou@gmail.com>
2024-07-10 10:06:53 +0100

    first

--- src/routing/ErrorPage.js
+++ src/routing/ErrorPage.js
@@ -1,10 +1,22 @@
-import React from 'react';
-import { isRouteErrorResponse, useRouteError } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import {
+  isRouteErrorResponse,
+  useNavigate,
+  useRouteError,
+} from 'react-router-dom';
 
 function ErrorPage() {
   const error = useRouteError();
 
   console.log(isRouteErrorResponse(error));
+
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    setTimeout(() => {
+      navigate('/');
+    }, 5000);
+  }, []);
 
   return (
     <div>
